3fa6e8f6417af6a35e95e1da82f0d3cc3345a021
unknown <facturs@mail.com>
2022-12-06 19:17:34 +0700

    homework5

---
+++ task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] PrintArray(int num)
+{
+    {
+        int[] array = new int[num];
+        for (int i = 0; i < num; i++)
+        {
+            array[i] = new Random().Next(-100, 100);
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+        return array;
+    }
+}
+
+void oddnums(int[] array)
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el % 2 == 0) count++;
+    }
+
+    Console.WriteLine($"В массиве {count} чётных чисел.");
+}
+
+Console.WriteLine("Введите размерность массива");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] arr = PrintArray(length);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов стоящих на нечетных позициях = {oddnums}.");
---
+++ task3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] PrintArray(int num)
+{
+    double[] array = new double[num];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100) + new Random().NextDouble();
+        Console.Write(Math.Round(array[i], 2) + " ");
+    }
+    return array;
+}
+
+double realnum(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    foreach (double el in array)
+    {
+        if (el > max) max = el;
+        if (el < min) min = el;
+    }
+    double diff = max - min;
+    return Math.Round(diff, 2);
+}
+
+
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = PrintArray(size);
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива = {realnum(array)}.");
